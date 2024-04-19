24dfa7a5c5af8b2dfeb756307fb5ea393ab140ab
lukunkun <kunkun@7net.cc>
2024-04-19 17:08:03 +0800

    build: 增加代码压缩

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['lib/index.ts'],
   format: ['cjs', 'esm'],
+  minify: true,
   dts: true,
   splitting: false,
   clean: true,
